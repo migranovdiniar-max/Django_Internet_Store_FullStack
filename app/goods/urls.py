1c663c20dbec9f1dfe7a45783011f0e14d438b94
Dinyar <migranovdiniar@gmail.com>
2025-12-19 08:16:00 +0300

    slug converter for product

--- app/goods/urls.py
+++ app/goods/urls.py
@@ -8,7 +8,8 @@ app_name = "goods"
 
 urlpatterns = [
     path('', views.catalog, name="index"),
-    path('product/', views.product, name="product"),
+    path('product/<int:product_id>/', views.product, name="product"),
+    path('product/<slug:product_slug>/', views.product, name="product"),
 ]
 
 if settings.DEBUG:
--- app/goods/views.py
+++ app/goods/views.py
@@ -14,5 +14,17 @@ def catalog(request):
     return render(request, 'goods/catalog.html', context)
 
 
-def product(request):
-    return render(request, 'goods/product.html')
+def product(request, product_slug=False, product_id=False):
+    if product_slug:
+        product = Products.objects.get(slug=product_slug)
+    elif product_id:
+        product = Products.objects.get(id=product_id)
+    else:
+        return render(request, 'goods/product.html', context={"title": "Product not found", "product": None})
+
+    context = {
+        "title": product.name,
+        "product": product
+    }
+
+    return render(request, 'goods/product.html', context)
