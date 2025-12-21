e7ba2ed5918c95e9ff60d8a0c1b2bcbabbe4d2aa
Dinyar <migranovdiniar@gmail.com>
2025-12-21 18:25:44 +0300

    connectrd postgresql, full text reserch

--- app/goods/utils.py
+++ app/goods/utils.py
@@ -1,16 +1,24 @@
 from goods.models import Products
 
 from django.db.models import Q
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 
 def q_search(query):
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
+    
+    vector = SearchVector('name', "description")
+    query = SearchQuery(query)
 
-    keywords = [word for word in query.split() if len(word) > 2]
+    return Products.objects.annotate(
+        rank=SearchRank(vector, query)
+    ).order_by('-rank')
 
-    q_objects = Q()
-    for token in keywords:
-        q_objects |= Q(name__icontains=token) | Q(description__icontains=token)
+    # keywords = [word for word in query.split() if len(word) > 2]
 
-    return Products.objects.filter(q_objects)
+    # q_objects = Q()
+    # for token in keywords:
+    #     q_objects |= Q(name__icontains=token) | Q(description__icontains=token)
+
+    # return Products.objects.filter(q_objects)
