0ab06903703d3b8dc77e8a833c3a50ab465e8089
Dinyar <migranovdiniar@gmail.com>
2025-12-21 17:45:29 +0300

    created fixturex, deleted sqlite3

--- app/goods/views.py
+++ app/goods/views.py
@@ -2,15 +2,19 @@ from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 from django.core.paginator import Paginator
 
+from goods.utils import q_search
 from goods.models import Categories, Products
 
 
-def catalog(request, category_slug):
+def catalog(request, category_slug=None):
     on_sale = request.GET.get('on_sale', None)
     order_by = request.GET.get('order_by', None)
+    query = request.GET.get('q', None)
 
     if category_slug == 'all-goods':
         goods = Products.objects.all()
+    elif query:
+        goods = q_search(query)
     else:
         category = get_object_or_404(Categories, slug=category_slug)
         goods = Products.objects.filter(category=category)
